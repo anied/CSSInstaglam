b88d50cfd2792c07968b8d8f1f4e7456228b5cdf
Alexander Nied <awnied@gmail.com>
2015-09-18 16:42:31 -0500

    Created a build process A little cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         watch: {
             css: {
                 files: "src/less/*.less",
@@ -13,15 +14,58 @@ module.exports = function(grunt) {
                     paths: ["src/less"]
                 },
                 files: {
-                    "src/css/style.css": "src/less/style.less"
+                    "src/css/cssinstaglam.dev.css": "src/less/cssinstaglam.dev.less"
                 }
             },
+        },
+        clean: {
+            dist: ['dist/*']
+        },
+        copy: {
+            build: {
+                src: 'src/css/cssinstaglam.dev.css',
+                dest: 'dist/cssinstaglam.css'
+            }
+        },
+        'string-replace' : {
+            version: {
+              files: {
+                'dist/cssinstaglam.css': 'dist/cssinstaglam.css'
+              },
+              options: {
+                replacements: [{
+                  pattern: /{{ VERSION }}/g,
+                  replacement: '<%= pkg.version %>'
+                }]
+              }
+            }
+        },
+        cssmin: {
+          target: {
+            options: {
+                keepSpecialComments: 1
+            },
+            files: [{
+              expand: true,
+              cwd: 'dist',
+              src: ['*.css', '!*.min.css'],
+              dest: 'dist',
+              ext: '.min.css'
+            }]
+          }
         }
     });
 
+
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-string-replace');
+
 
     grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['clean', 'copy:build', 'string-replace:version', 'cssmin']);
 
 };
